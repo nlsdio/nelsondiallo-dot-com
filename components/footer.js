28502d3d51d0f4811fdf6b4574732aabded6d9dd
nlsdio <52054460+nlsdio@users.noreply.github.com>
2023-01-17 20:12:14 -0800

    amber

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://github.com/Netlify-Moneytronic"
-              className="mx-3 bg-black hover:bg-blue-300 hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-amber-600 hover:bg-blue-300 hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Read Documentation
             </a>
